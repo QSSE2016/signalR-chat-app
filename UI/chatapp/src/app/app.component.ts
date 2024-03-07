31ea501adc3fe8cc659672571c526c960ed417be
Alexandros <alexstathas1996@gmail.com>
2024-03-07 22:03:14 +0200

    Moved connection code to main component (app) and refactored the code for more clarity.

--- UI/chatapp/src/app/app.component.ts
+++ UI/chatapp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HubConnection } from '@microsoft/signalr';
+import { ClientService } from './services/client.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatapp';
+  clientConnection?: HubConnection
+
+  constructor(private client: ClientService) {}
+
+  // When submitting to the waiting room form.
+  async connectToRoom(form: FormGroup) {
+    this.setupConnection()
+    await this.startConnection(form)
+  }
+
+
+  // SignalR Client Connection Functions
+  
+  private setupConnection() {
+    this.clientConnection = this.client.buildConnection()
+    
+    // Setup a client-side handler for incoming messages sent from the server. (specifically messages with the name/method "ReceiveMessage")
+    this.clientConnection.on("ReceiveMessage",(username,msg) => {
+      console.log("username: ", username)
+      console.log("msg: ",msg)
+    })
+
+    // For messages sent by users
+    this.clientConnection.on("ReceiveGeneralMessage",(username,msg) => {
+       console.log("USERNAME: ",username)
+       console.log("MSG: ",msg)
+    })
+  }
+  private async startConnection(form: FormGroup) {
+    try {
+      await this.clientConnection?.start()
+      alert("Connection started!")
+
+      const payload = {
+        username: form.controls['username'].value,
+        roomtojoin: form.controls['chatroom'].value
+      }
+
+      await this.clientConnection?.invoke("JoinChatRoom",payload)  
+    } catch (error) {
+      console.log("OOPS ERROR DETECTED: ",error)
+    }
+  }
 }
--- UI/chatapp/src/app/components/waiting-room.component.ts
+++ UI/chatapp/src/app/components/waiting-room.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { HubConnection } from '@microsoft/signalr';
 import { ClientService } from '../services/client.service';
 
 @Component({
@@ -10,60 +9,21 @@ import { ClientService } from '../services/client.service';
 })
 export class WaitingRoomComponent{
   form: FormGroup
-  clientConnection?: HubConnection
+  @Output() submitEvent = new EventEmitter<FormGroup>();
 
-  constructor(private client: ClientService) {
+  constructor() {
     this.form = new FormGroup({
       username: new FormControl('',Validators.required),
       chatroom: new FormControl('',Validators.required)
     })
   }
-
-  async onSubmit() {
+  
+  onSubmit() {
     if(this.form.invalid) {
       alert("Please fill out all the fields.")
       return
     }
 
-
-    this.setupConnection()
-    await this.startConnection()
+    this.submitEvent.emit(this.form)
   }
-
-  // Again, this will be moved later, just makign sure everything works as intented
-  setupConnection() {
-    // this code will be moved elsewhere probably
-    this.clientConnection = this.client.buildConnection()
-    
-    // Setup a client-side handler for incoming messages sent from the server. (specifically messages with the name/method "ReceiveMessage")
-    this.clientConnection.on("ReceiveMessage",(username,msg) => {
-      console.log("username: ", username)
-      console.log("msg: ",msg)
-    })
-
-    // For messages sent
-    this.clientConnection.on("ReceiveGeneralMessage",(username,msg) => {
-       console.log("USERNAME: ",username)
-       console.log("MSG: ",msg)
-    })
-  }
-
-  async startConnection() {
-    try {
-      await this.clientConnection?.start()
-      alert("Connection started!")
-
-      const payload = {
-        username: this.form.controls['username'].value,
-        roomtojoin: this.form.controls['chatroom'].value
-      }
-
-      await this.clientConnection?.invoke("JoinChatRoom",payload)  
-    } catch (error) {
-      console.log("OOPS ERROR DETECTED: ",error)
-    }
-  }
-
-
-  
 }
